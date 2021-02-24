81b871e15b660d571f662ee42b21c66d49895fff
Anna Grinenko <very.little.anna@gmail.com>
2021-02-24 21:20:07 +0300

    generate_double_array() was fixed.

--- main.c
+++ main.c
@@ -4,20 +4,21 @@
 #include <time.h>
 
 
-int cmp(const double * a, const double * b)
+int cmp(const void * a, const void * b)
 {
-    if (*a < *b) return -1;
-    if (*a > *b) return 1;
+    if (fabs(*(const double *)a) < fabs(*(const double *)b)) return -1;
+    if (fabs(*(const double *)a) > fabs(*(const double *)b)) return 1;
     return 0;
 }
 
-int inverse_cmp(const double * a, const double * b) /// for sorting in reverse order in fuction check()
+int inverse_cmp(const void * a, const void * b) /// for sorting in reverse order in fuction check()
 {
-    if (*a > *b) return -1;
-    if (*a < *b) return 1;
+    if (fabs(*(const double *)a) > fabs(*(const double *)b)) return -1;
+    if (fabs(*(const double *)a) < fabs(*(const double *)b)) return 1;
     return 0;
 }
-int print_array(int len, double * a)
+
+void print_array(int len, double * a)
 {
     for (int i = 0; i < len; ++i)
         printf("%.5f ", a[i]);
@@ -34,7 +35,7 @@ void shell_sort(int n, double * a, int * p_cnt_of_comparisons, int * p_cnt_of_sw
             for (int j = i + h; j < n; j+= h)
             {
                 cnt_of_comparisons++;
-                if (a[j] < a[i])
+                if (fabs(a[j]) < fabs(a[i]))
                 {
                     cnt_of_swaps++;
                     double tmp = a[i];
@@ -55,9 +56,9 @@ void sift_down(int n, int i, double * heap, int * p_cnt_of_comparisons, int * p_
     {
         /// sons of i: 2*i+1 ans 2*i+2
         int max_son = i; ///index of the greatest son
-        if (2 * i + 1 < n && heap[2 * i + 1] > heap[i])
+        if (2 * i + 1 < n && fabs(heap[2 * i + 1]) > fabs(heap[i]))
             max_son = 2 * i + 1;
-        if (2 * i + 2 < n && heap[2 * i + 2] > heap[max_son]) ///if right son exists and is the greatest
+        if (2 * i + 2 < n && fabs(heap[2 * i + 2]) > fabs(heap[max_son])) ///if right son exists and is the greatest
             max_son = 2 * i + 2;
         (*p_cnt_of_comparisons) += 2;
         if (max_son == i) ///heap is correct (no sons or they are smaller)
@@ -83,11 +84,12 @@ void heap_sort(int n, double * a, int * p_cnt_of_comparisons, int * p_cnt_of_swa
         a[0] = a[heap_size - 1];
         a[heap_size - 1] = tmp;
         heap_size--; ///add maximun to the end and sift new root
+        ++*p_cnt_of_swaps;
         sift_down(heap_size, 0, a, p_cnt_of_comparisons, p_cnt_of_swaps);
     }
 }
 
-void generate_double_array(int n, double * a, double * b, double * c, double * e, int type) ///1 - sorted, 2 - reverse sorted, 3,4 - random order
+void generate_double_array(int n, double * a, double * b, double * c, int type) ///1 - sorted, 2 - reverse order, 3,4 - random order
 {
     union
     {
@@ -108,12 +110,11 @@ void generate_double_array(int n, double * a, double * b, double * c, double * e
     else if(type == 2)
         qsort(a, n, sizeof(double), inverse_cmp);
     for (int i = 0; i < n; ++i)
-        b[i] = c[i] = e[i] = a[i];
+        b[i] = c[i] = a[i];
 }
 
 int check(int n, double * origin, double * result)
 {
-    qsort(origin, n, sizeof(double), cmp);
     for (int i = 0; i < n; ++i)
         if (origin[i] != result[i]) return 0;
     return 1;
@@ -121,29 +122,38 @@ int check(int n, double * origin, double * result)
 
 void test(int n)
 {
-    double * origin1 = malloc(sizeof(double) * n); ///for checking result of sort 1
-    double * origin2 = malloc(sizeof(double) * n); ///for checking result of sort 2
+    double * origin = malloc(sizeof(double) * n); ///for checking result of sort 1
     double * set1 = malloc(sizeof(double) * n);
     double * set2 = malloc(sizeof(double) * n);
+    srand(time(NULL));
     for (int type_of_data = 1; type_of_data <= 4; ++type_of_data)
     {
-        srand(time(NULL));
-        generate_double_array(n, origin1, origin2, set1, set2, type_of_data);
+        generate_double_array(n, origin, set1, set2, type_of_data);
+        qsort(origin, n, sizeof(double), cmp);
+        switch (type_of_data)
+        {
+            case 1:
+                printf("-> sorted:\n");
+                break;
+            case 2:
+                printf("-> reverse order\n");
+                break;
+            default: printf("-> random\n");
+        }
         ///1
         int cnt1 = 0, cnt2 = 0;
         shell_sort(n, set1, &cnt1, &cnt2);
-        if (!check(n, origin1, set1))
+        if (!check(n, origin, set1))
             fprintf(stderr, "Error: Wrong Shell sort\n");
-        printf("Shell: comp = %d, swaps = %d\n", cnt1, cnt2);
+        printf("Shell sort: comp = %d, swaps = %d\n", cnt1, cnt2);
         ///2
         cnt1 = cnt2 = 0;
         heap_sort(n, set2, &cnt1, &cnt2);
-        if (!check(n, origin2, set2))
+        if (!check(n, origin, set2))
             fprintf(stderr, "Error: Wrong heap sort\n");
-        printf("Heap sort: comp = %d, swaps = %d\n", cnt1, cnt2);
+        printf("Heap sort: comp = %d, swaps = %d\n\n", cnt1, cnt2);
     }
-    free(origin1);
-    free(origin2);
+    free(origin);
     free(set1);
     free(set2);
 }
